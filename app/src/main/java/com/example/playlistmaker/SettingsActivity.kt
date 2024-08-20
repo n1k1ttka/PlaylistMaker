b99b6506b6fc8f41b0b878c371d7041f0074d179
n1k1ttka <nikita.kolgushkin@mail.ru>
2024-08-20 20:42:53 +0300

    Added colors and styles for day/night mode

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,12 +1,15 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -24,5 +27,23 @@ class SettingsActivity : AppCompatActivity() {
         val backBttn = findViewById<ImageView>(R.id.back)
         val backClickListener: View.OnClickListener = View.OnClickListener { finish() }
         backBttn.setOnClickListener(backClickListener)
+
+        val modeSwitch = findViewById<SwitchCompat>(R.id.mode_switch)
+
+        modeSwitch.isChecked =
+            when (resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> true
+            else -> false
+        }
+
+        modeSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            AppCompatDelegate.setDefaultNightMode(
+                if (isChecked) {
+                    AppCompatDelegate.MODE_NIGHT_YES
+                } else {
+                    AppCompatDelegate.MODE_NIGHT_NO
+                }
+            )
+        }
     }
 }
