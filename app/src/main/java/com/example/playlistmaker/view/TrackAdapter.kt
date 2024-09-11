b244a75722f3ee85dc83686080e6b9266e48ce43
n1k1ttka <nikita.kolgushkin@mail.ru>
2024-09-11 21:16:19 +0300

    Add RecyclerView in SearchActivity

---
+++ app/src/main/java/com/example/playlistmaker/view/TrackAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.playlistmaker.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.playlistmaker.R
+import com.example.playlistmaker.model.Track
+
+class TrackAdapter(
+    private val tracks: List<Track>
+): RecyclerView.Adapter<TrackViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        return TrackViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.song_item, parent, false))
+    }
+
+    override fun getItemCount(): Int = tracks.size
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        holder.bind(tracks[position])
+    }
+
+}
