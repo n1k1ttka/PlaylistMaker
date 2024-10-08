4959828f6ce97c6980908aa9ce474ca1a3e7236a
n1k1ttka <nikita.kolgushkin@mail.ru>
2024-10-08 12:49:24 +0300

    Add Retrofit in Project

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,14 +1,20 @@
 package com.example.playlistmaker
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,25 +22,29 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.playlistmaker.model.Track
+import com.example.playlistmaker.remote.ITunesApiService
+import com.example.playlistmaker.remote.ITunesResponce
 import com.example.playlistmaker.view.TrackAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class SearchActivity : AppCompatActivity() {
 
     private lateinit var inputEditText: EditText
     private lateinit var textValue: String
+    private lateinit var recycler: RecyclerView
+    private lateinit var placeholder: ViewGroup
+    private lateinit var placeholderImage: ImageView
+    private lateinit var placeholderMessage: TextView
+    private lateinit var placeholderButton: Button
+    private lateinit var iTunesService: ITunesApiService
 
-    private val songs = arrayListOf<Track>(
-        Track("Smells Like Teen Spirit","Nirvana","5:01",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-        Track("Billie Jean","Michael Jackson","4:35",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-        Track("Stayin' Alive","Bee Gees","4:10",
-            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-        Track("Whole Lotta Love","Led Zeppelin","5:33",
-            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg\n"),
-        Track("Sweet Child O'Mine","Guns N' Roses","5:03",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg \n"),
-    )
+    private val songs = ArrayList<Track>()
+    private val adapter = TrackAdapter(songs)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,9 +56,16 @@ class SearchActivity : AppCompatActivity() {
             insets
         }
 
-        val recycler = findViewById<RecyclerView>(R.id.track_recycler_view)
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://itunes.apple.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        iTunesService = retrofit.create<ITunesApiService>()
+
+        recycler = findViewById(R.id.track_recycler_view)
         recycler.layoutManager = LinearLayoutManager(this)
-        recycler.adapter = TrackAdapter(songs)
+        recycler.adapter = adapter
 
         val backBttn = findViewById<ImageView>(R.id.back)
         val backClickListener: View.OnClickListener = View.OnClickListener { finish() }
@@ -56,6 +73,10 @@ class SearchActivity : AppCompatActivity() {
 
         val clearButton = findViewById<ImageButton>(R.id.clear_button)
         inputEditText = findViewById(R.id.searching)
+        placeholder = findViewById(R.id.placeholderMessage)
+        placeholderImage = findViewById(R.id.placeholderImage)
+        placeholderMessage = findViewById(R.id.error_text)
+        placeholderButton = findViewById(R.id.refresh)
 
         clearButton.setOnClickListener {
             inputEditText.setText("")
@@ -80,6 +101,17 @@ class SearchActivity : AppCompatActivity() {
             }
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                remoteRequest()
+                true
+            }
+            false
+        }
+
+        placeholderButton.setOnClickListener{
+            remoteRequest()
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -98,6 +130,53 @@ class SearchActivity : AppCompatActivity() {
         } else {
             View.VISIBLE
         }
+    }
+
+    private fun showMessage(text: String, image: Drawable?, additionalMessage: String) {
+        if (text.isNotEmpty()) {
+            recycler.visibility = View.GONE
+            placeholder.visibility = View.VISIBLE
+            songs.clear()
+            adapter.notifyDataSetChanged()
+            placeholderImage.setImageDrawable(image)
+            placeholderMessage.text = text
+            if (additionalMessage.isNotEmpty()) {
+                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
+                    .show()
+                placeholderButton.visibility = View.VISIBLE
+            } else {
+                placeholderButton.visibility = View.GONE
+            }
+        } else {
+            placeholder.visibility = View.GONE
+            recycler.visibility = View.VISIBLE
+        }
+    }
+
+    private fun remoteRequest(){
+        iTunesService.search(inputEditText.text.toString()).enqueue(object : Callback<ITunesResponce> {
+            override fun onResponse(
+                call: Call<ITunesResponce>,
+                response: Response<ITunesResponce>
+            ) {
+                if(response.code() == 200) {
+                    songs.clear()
+                    if(response.body()?.results?.isNotEmpty() == true) {
+                        songs.addAll(response.body()?.results!!)
+                        adapter.notifyDataSetChanged()
+                        showMessage("", null, "")
+                    } else {
+                        showMessage(getString(R.string.nothing_was_found), getDrawable(R.drawable.res_not_ex), "")
+                    }
+                } else {
+                    showMessage(getString(R.string.no_internet), getDrawable(R.drawable.no_internet), response.code().toString())
+                }
+            }
+
+            override fun onFailure(call: Call<ITunesResponce>, t: Throwable) {
+                showMessage(getString(R.string.no_internet), getDrawable(R.drawable.no_internet), t.message.toString())
+            }
+        })
     }
 
     companion object {
---
+++ app/src/main/java/com/example/playlistmaker/remote/ITunesApiService.kt
@@ -0,0 +1,12 @@
+package com.example.playlistmaker.remote
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface ITunesApiService {
+
+    @GET("/search")
+    fun search(@Query("term") text: String): Call<ITunesResponce>
+}
